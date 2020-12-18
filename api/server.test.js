a6a7b746adef8dec0455a851807d5d1b8d2d0063
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-12-17 21:57:16 -0500

    testing file added

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,18 @@
 // Write your tests here
+
+const request = require('supertest');
+const server = require('./server'); // a router
+const Users = require('./users/user-model');
+
+const newUser = { name: 'user1', password: '123456' };
+
 test('sanity', () => {
-  expect(true).toBe(false)
-})
+  expect(true).toBe(false);
+});
+
+describe('[POST] api/users', () => {
+  it('resturns the newly created user', async () => {
+    const res = await request(server).post('api/users').send(newUser);
+    expect(res.body.username).toBe('user1');
+  });
+});
